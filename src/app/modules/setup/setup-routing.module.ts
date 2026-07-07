aec59fd43c69adf3d6bfeb11f214fd6e9c5f2e93
Prashan Silva <prashansilva@gmail.com>
2026-07-07 21:50:48 +0530

    Angular forms reactive forms

--- src/app/modules/setup/setup-routing.module.ts
+++ src/app/modules/setup/setup-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SetupComponent} from "./components/setup/setup.component";
+import {ReactiveSetupComponent} from "./components/reactive-setup/reactive-setup.component";
 
 const routes: Routes = [
   { path: '', component: SetupComponent },
+  { path: 'reactive', component: ReactiveSetupComponent },
   { path :'**' , redirectTo: ''}
 ];
 
--- src/app/modules/setup/setup.module.ts
+++ src/app/modules/setup/setup.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupComponent } from './components/setup/setup.component';
 import {SetupRoutingModule} from "./setup-routing.module";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { CustomValidationDirective } from './directives/custom-validation.directive';
+import { ReactiveSetupComponent } from './components/reactive-setup/reactive-setup.component';
 
 
 
 @NgModule({
   declarations: [
     SetupComponent,
-    CustomValidationDirective
+    CustomValidationDirective,
+    ReactiveSetupComponent
   ],
   imports: [
     CommonModule,
     SetupRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SetupModule { }
