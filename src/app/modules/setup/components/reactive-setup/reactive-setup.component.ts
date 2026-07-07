a4161e2cdc227c2800af5237c3cddbf54c894a43
Prashan Silva <prashansilva@gmail.com>
2026-07-07 22:39:39 +0530

    Angular forms reactive forms

--- src/app/modules/setup/components/reactive-setup/reactive-setup.component.ts
+++ src/app/modules/setup/components/reactive-setup/reactive-setup.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Component} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-setup',
@@ -21,6 +21,24 @@ export class ReactiveSetupComponent {
     }
   )
 
+  applicationForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required]),
+      address: new FormGroup(
+        {
+          street: new FormControl('', [Validators.required]),
+          city: new FormControl('', [Validators.required]),
+          country: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+          zip: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+        }
+      ),
+      skills: new FormArray<SkillForm>(
+        [ this.createSkill() ]
+      )
+    }
+  )
+
   constructor() { }
 
   onSubmit() {
@@ -32,4 +50,43 @@ export class ReactiveSetupComponent {
       console.log( "Invalid" );
     }
   }
+
+  onSubmitApplication() {
+    this.applicationForm.markAsTouched();
+    console.log( this.applicationForm)
+    if( this.applicationForm.valid) {
+      console.log( "Valid" );
+      console.log( this.applicationForm.value );
+    } else {
+      console.log( "Invalid" );
+    }
+  }
+
+  createSkill(): SkillForm {
+    return new FormGroup({
+      name: new FormControl('', {
+        nonNullable: true,
+        validators: Validators.required
+      }),
+      value: new FormControl('', {
+        nonNullable: true,
+        validators: Validators.required
+      })
+    });
+  }
+
+  getSkills() {
+    return this.applicationForm.controls.skills.controls;
+  }
+
+  addNewSkill() {
+    this.applicationForm.controls.skills.push(this.createSkill());
+  }
+
 }
+
+
+type SkillForm = FormGroup<{
+  name: FormControl<string>;
+  value: FormControl<string>;
+}>;
